b616c2059e00ea6734c60c453ddf86dcf2fd9422
PRANAY BHARGAV BHAVARAJU <quic_pbhavara@quicinc.com>
2022-08-09 13:48:47 +0530

    sm8475-common: Add UID for configuring Thales eSE keymint
    Add UID for Thales eSE keymint module in
    order to bind OMAPI service to keymint HAL.
    
    Change-Id: I22f8bf1558facfd4fa57808df9200cd2d54b3997

--- config.fs
+++ config.fs
@@ -34,6 +34,9 @@ value:2911
 [AID_VENDOR_SSGTZD]
 value:2912
 
+[AID_VENDOR_THALES_STRONGBOX]
+value:2913
+
 [AID_VENDOR_BRIDGE_RW]
 value:5001
 
@@ -57,6 +60,12 @@ value:5120
 
 [AID_VENDOR_PROD_PERSIST]
 value:5121
+
+[vendor/bin/hw/android.hardware.security.keymint-service.strongbox-thales]
+mode: 0755
+user: AID_VENDOR_THALES_STRONGBOX
+group: AID_SYSTEM
+caps: SYS_ADMIN SYS_NICE
 
 [vendor/bin/hw/android.hardware.security.keymint-service.strongbox]
 mode: 0755
