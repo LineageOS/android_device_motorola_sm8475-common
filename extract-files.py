b317517f36c59a17e2734f68056e7dc91c39c089
Bruno Martins <bgcngm@gmail.com>
2024-10-01 22:35:45 +0100

    sm8475-common: Address wfdhdcphalservice seccomp filter failures
    Fixes:
     W wfdhdcphalservice: libminijail[5312]: logging seccomp filter failures
     E wfdhdcphalservice: libminijail[5312]: blocked syscall: gettid
     I init    : Service 'wfdhdcphalservice' (pid 5312) received signal 31
     I init    : Sending signal 9 to service 'wfdhdcphalservice' (pid 5312) process group...
    
    Change-Id: Ic259464520db6e77f06a3d0820a63662aa91659c

--- extract-files.py
+++ extract-files.py
@@ -95,6 +95,8 @@ blob_fixups: blob_fixups_user_type = {
     ),
     'vendor/etc/seccomp_policy/atfwd@2.0.policy': blob_fixup()
         .add_line_if_missing('gettid: 1'),
+    'vendor/etc/seccomp_policy/wfdhdcphalservice.policy': blob_fixup()
+        .add_line_if_missing('gettid: 1'),
 }
 
 module = ExtractUtilsModule(
