1d9658378fbdbd3baf16d7a9c6cc07cc5334bfa8
Cosmin Tanislav <demonsingur@gmail.com>
2025-01-28 03:18:57 +0200

    sm8475-common: move vibrator service to device
    Change-Id: I256ee77bea74775fa084bfb8e5522378a5d1346e

--- sm8475.mk
+++ sm8475.mk
@@ -369,9 +369,6 @@ PRODUCT_PACKAGES += \
     vndservicemanager
 
 # Vibrator
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator.service
-
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
