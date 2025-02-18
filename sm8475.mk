13746f0bacf29fbaa357fbab03a424d7073b9835
Cosmin Tanislav <demonsingur@gmail.com>
2025-02-18 19:24:04 +0200

    Revert "sm8475-common: Migrate to QTI USB Gadget AIDL HAL"
    This reverts commit fc6c5cec5c419dc20ba1e164792af2bc07a648ac.
    
    Change-Id: If9d4b2edf714d088415a1bbb99ab330aec9a15f0

--- sm8475.mk
+++ sm8475.mk
@@ -355,11 +355,9 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb-service.qti \ \
-    android.hardware.usb.gadget-service.qti \
+    android.hardware.usb-service.qti \
     init.qcom.usb.rc \
-    init.qcom.usb.sh \
-    usb_compositions.conf
+    init.qcom.usb.sh
 
 PRODUCT_SOONG_NAMESPACES += vendor/qcom/opensource/usb/etc
 
