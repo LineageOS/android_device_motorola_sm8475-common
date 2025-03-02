714d7571e8ce5cadc16cecda0d3e48d49d104cab
Cosmin Tanislav <demonsingur@gmail.com>
2025-03-02 18:20:16 +0200

    Revert "sm8475-common: move vibrator service to device"
    This reverts commit 52841ed97b4c5a4f9d59000fc82ccfbaf4d2536a.
    
    Change-Id: I156afd1d27aa7fca34f66236b2aa3b9cd77db206

--- sm8475.mk
+++ sm8475.mk
@@ -376,6 +376,9 @@ PRODUCT_PACKAGES += \
     vndservicemanager
 
 # Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator.service
+
 PRODUCT_COPY_FILES += \
     vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
