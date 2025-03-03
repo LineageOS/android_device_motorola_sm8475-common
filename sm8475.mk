4c2afce75bcf4704ea9d7e94c8c32cab955870a3
Cosmin Tanislav <demonsingur@gmail.com>
2025-03-03 12:04:51 +0200

    sm8475-common: use strength-specific effects
    Change-Id: Ibaacbd8b73ff9e2f1359a7e722224e4461d554f8

--- sm8475.mk
+++ sm8475.mk
@@ -379,6 +379,7 @@ PRODUCT_PACKAGES += \
 # Vibrator
 $(call soong_config_set,qti_vibrator,effect_lib,libqtivibratoreffect.moto_sm8475-richtap)
 $(call soong_config_set,qti_vibrator,use_effect_stream,true)
+$(call soong_config_set,qti_vibrator,use_effect_stream_strength,true)
 
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.vibrator.service
