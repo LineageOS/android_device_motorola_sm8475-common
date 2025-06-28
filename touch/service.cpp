53d71d1521a73c2eac782ff052519e46ca1abcb7
Nolen Johnson <johnsonnolen@gmail.com>
2025-06-28 13:37:09 -0400

    sm8475-common: Use Moto AIDL Lineage Touch HAL for HTP
    * Keep our HIDL Touch HAL around purely for Touchscreen gestures.
    * Will rework this at some point into an AIDL.
    
    Change-Id: I01d573f6ac854aa15935b1030808c0cf11bdcbd4

--- touch/service.cpp
+++ touch/service.cpp
@@ -19,34 +19,25 @@
 #include <android-base/logging.h>
 #include <hidl/HidlTransportSupport.h>
 
-#include "HighTouchPollingRate.h"
 #include "TouchscreenGesture.h"
 
-using ::vendor::lineage::touch::V1_0::IHighTouchPollingRate;
 using ::vendor::lineage::touch::V1_0::ITouchscreenGesture;
-using ::vendor::lineage::touch::V1_0::implementation::HighTouchPollingRate;
 using ::vendor::lineage::touch::V1_0::implementation::TouchscreenGesture;
 
 int main() {
-    android::sp<IHighTouchPollingRate> highTouchPollingRate = new HighTouchPollingRate();
     android::sp<ITouchscreenGesture> touchscreenGesture = new TouchscreenGesture();
 
     android::hardware::configureRpcThreadpool(1, true /*callerWillJoin*/);
-
-    if (highTouchPollingRate->registerAsService() != android::OK) {
-        LOG(ERROR) << "Cannot register touchscreen high touch polling HAL service.";
-        return 1;
-    }
 
     if (touchscreenGesture->registerAsService() != android::OK) {
         LOG(ERROR) << "Cannot register touchscreen gesture HAL service.";
         return 1;
     }
 
-    LOG(INFO) << "Touchscreen HAL service ready.";
+    LOG(DEBUG) << "Touchscreen Gesture HAL service ready.";
 
     android::hardware::joinRpcThreadpool();
 
-    LOG(ERROR) << "Touchscreen HAL service failed to join thread pool.";
+    LOG(ERROR) << "Touchscreen HAL Gesture service failed to join thread pool.";
     return 1;
 }
