8488e50a852321d9c6a6337e796976fb2f9b840d
Nikita Savchenko <NikitaSavc@yandex.ru>
2025-07-13 19:11:19 +0000

    sm8475-common: Remove dolby references in media configurations
    Change-Id: I47374995db148f4451dbe45b9de19dc0be626103

--- extract-files.py
+++ extract-files.py
@@ -99,7 +99,7 @@ blob_fixups: blob_fixups_user_type = {
         'vendor/etc/seccomp_policy/sensors-qesdk.policy',
     ): blob_fixup().add_line_if_missing('gettid: 1'),
     'vendor/etc/media_codecs_cape.xml': blob_fixup().regex_replace(
-        '.*media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio).*\n', ''
+        '.*media_codecs_(google_audio|google_c2|google_telephony|google_video|vendor_audio|dolby_audio).*\n', ''
     ),
 }
 
